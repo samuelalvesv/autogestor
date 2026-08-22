7020f1af49d515041f55236fcad9c4690c6bf823
Samuel Alves <samuelavieira@outlook.com>
2026-08-22 19:44:50 -0300

    add propriedades do Response.cs

--- src/Autogestor.Contract/Responses/Response.cs
+++ src/Autogestor.Contract/Responses/Response.cs
@@ -1,6 +1,8 @@
 namespace Autogestor.Contract.Responses;
 
-public class Response
+public class Response(string data, string message, int code)
 {
-
+    public string? Data { get; set; } = data;
+    public string Message { get; set; } = message;
+    public bool IsSuccess => code is >= 200 and <= 299;
 }
