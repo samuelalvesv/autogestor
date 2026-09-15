b1b69afa0dafefc14fad7e9a7b1ca7a45e2cd756
Samuel Alves <samuelavieira@outlook.com>
2026-09-14 21:35:54 -0300

    feat: adiciona migração do ef core com suporte a multi-tenancy, índices de tenant e ajuste de nulabilidade nos campos de auditoria de categorias e transações

--- src/Autogestor.Infrastructure/Persistence/Migrations/20260915002557_AddMultiTenancyAndRefineAuditA.Designer.cs
+++ src/Autogestor.Infrastructure/Persistence/Migrations/20260915002557_AddMultiTenancyAndRefineAuditA.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Autogestor.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Autogestor.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260915002557_AddMultiTenancyAndRefineAuditA")]
+    partial class AddMultiTenancyAndRefineAuditA
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Autogestor.Infrastructure/Persistence/Migrations/20260915002557_AddMultiTenancyAndRefineAuditA.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Autogestor.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMultiTenancyAndRefineAuditA : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Transactions",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Categories",
+                newName: "TenantId");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Categories",
+                newName: "Title");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UpdatedBy",
+                table: "Transactions",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Transactions",
+                type: "timestamptz",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "Transactions",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UpdatedBy",
+                table: "Categories",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Categories",
+                type: "timestamptz",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_TenantId",
+                table: "Transactions",
+                column: "TenantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_TenantId",
+                table: "Categories",
+                column: "TenantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_TenantId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_TenantId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Transactions",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Categories",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "TenantId",
+                table: "Categories",
+                newName: "UserId");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UpdatedBy",
+                table: "Transactions",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Transactions",
+                type: "timestamptz",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UpdatedBy",
+                table: "Categories",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Categories",
+                type: "timestamptz",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz",
+                oldNullable: true);
+        }
+    }
+}
